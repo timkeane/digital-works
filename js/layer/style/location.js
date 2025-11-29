a26c6aba01d83c6c1478133b5d9d8dbaf297f579
Tim Keane <t.d.keane@gmail.com>
2025-11-29 04:00:37 -0500

    get fancy

--- js/layer/style/location.js
+++ js/layer/style/location.js
@@ -21,7 +21,9 @@ export default function(feature, resolution) {
   if (getFuture() === feature.get('has-future')) {
     const zoom = getView().getZoomForResolution(resolution);
     const people = feature.get('people') || 1;
-    const width = feature.get('highlight') ? 4 : 1.25;
+    const highlight = feature.get('highlight');
+    const width = highlight ? 4 : 1.25;
+    const color = highlight ? '#acdef2' : '#3399CC';
     let radius = zoom * people / 50;
     if (radius < 5) radius = 5;
     return new Style({
@@ -29,7 +31,7 @@ export default function(feature, resolution) {
       image: new Circle({
         radius,
         stroke: new Stroke({width, color: '#111a52'}),
-        fill: new Fill({color: '#3399CC'})
+        fill: new Fill({color})
       })
     });
   }
