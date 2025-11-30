79874c67a4f1c54ee56aa51fb60822b51fa36402
Tim Keane <t.d.keane@gmail.com>
2025-11-30 16:35:54 -0500

    css

--- js/html/banner/digital-works.js
+++ js/html/banner/digital-works.js
@@ -1,5 +1,5 @@
 export default `<div>
-  <img src="img/training/og.webp" data-i18n="[alt]app.name" style="position: fixed;left:-10000px">
+  <img src="img/digital-works/og.webp" data-i18n="[alt]app.name" style="position: fixed;left:-10000px">
   <div class="logoState">
     <img src="img/digital-works/logo.avif" data-i18n="[alt]banner.cn.alt">
   </div>
